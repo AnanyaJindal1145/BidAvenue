1a4038e73d93fd6b6b0c58c84a4b457846a5ae4b
AnanyaJindal1145 <ajindal1145@gmail.com>
2024-06-02 23:57:38 +0530

    added client folder

---
+++ Backend/client/src/components/CardData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        dish: "Paket Phillips Wrist Watch",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "MODEL s1800",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 11 AUCTIONS from here recently",
+        price: 35000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        dish: "Golden Idol- Kaali Maata",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "CHARITY",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 25 auctions from here recently",
+        price: 25000,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        dish: "Woman on a Swing- Painting",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "Artist- Shankar Rao",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 65 auctions from here recently",
+        price: 70000,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        dish: "Govrland Typwriter from 1935",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "Model GY3905",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 3 auctions from here recently",
+        price: 70000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        dish: "Bronze Light",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "CHARITY",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "10 auctions from here recently",
+        price: 210000,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        dish: "Ancient China Set",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "From 1384- Han Dunasty",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 11 auctions from here recently",
+        price: 100000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        dish: "Radio from  the 1970s",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "Model 1823g2",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "5 auctions from here recently",
+        price: 300000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        dish: "Wrist Watch - Rolex",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "Model z5- New",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "5 auctions from here recently",
+        price: 100000,
+        rating: "3.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        dish: "Golden Idol - Charity",
+        imgdata: "https://5.imimg.com/data5/SELLER/Default/2022/6/CV/SR/VR/105150937/af1-6-a.jpg",
+        address: "Burger, Sandwich, Fast Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2 auctions from here recently",
+        price: 10000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
